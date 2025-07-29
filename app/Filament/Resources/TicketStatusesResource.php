1d4ef05d373a7dd32f6bbc4a72dddb5a29d5dc7c
Alejandro Luzardo <alejandro@strix.com.ve>
2025-07-29 17:52:49 -0400

    feat(tickets resource): add buttons

--- app/Filament/Resources/TicketStatusesResource.php
+++ app/Filament/Resources/TicketStatusesResource.php
@@ -41,8 +41,15 @@ class TicketStatusesResource extends Resource
 			])
 			->filters([])
 			->actions([
+				Tables\Actions\EditAction::make(),
+				Tables\Actions\DeleteAction::make(),
+				Tables\Actions\ForceDeleteAction::make(),
+				Tables\Actions\RestoreAction::make(),
 			])
 			->bulkActions([
+				Tables\Actions\DeleteBulkAction::make(),
+				Tables\Actions\ForceDeleteBulkAction::make(),
+				Tables\Actions\RestoreBulkAction::make(),
 			]);
 	}
 
